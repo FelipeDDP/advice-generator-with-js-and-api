f2224033a33f93dd168d44bed6a249c928b40373
Felipe de Paula <fdiasdepaula@gmail.com>
2024-03-20 14:08:49 +0100

    Refactored JavaScript

--- src/js/index.js
+++ src/js/index.js
@@ -1,19 +1,12 @@
-const btnNewAdvice = document.querySelector('.btn-new-advice');
 const adviceId = document.getElementById('advice-id');
 const randomAdvice = document.getElementById('advice');
 
 async function selectNewRandomAdvice() {
-    const url = "https://api.adviceslip.com/advice";
-    const response = await fetch(url);
-    const json = await response.json()
-    console.log(json)
-    console.log(json.slip.id)
-    adviceId.innerHTML = json.slip.id
-    randomAdvice.innerHTML = json.slip.advice
+    const { slip: { id, advice } } = await (await fetch("https://api.adviceslip.com/advice")).json()
+    adviceId.innerHTML = id
+    randomAdvice.innerHTML = advice
 }
 
 selectNewRandomAdvice()
 
-btnNewAdvice.addEventListener('click', () => {
-    selectNewRandomAdvice()
-})
+document.querySelector('.btn-new-advice').addEventListener('click', selectNewRandomAdvice);
